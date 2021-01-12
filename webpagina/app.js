d4df454362b9da98e31d2e60dd09b92516f4fb2a
Bart <Bartdb2001@gmail.com>
2021-01-12 15:25:08 +0100

    Kamer, modellen eringezet

--- webpagina/app.js
+++ webpagina/app.js
@@ -2,17 +2,22 @@
 const controls = new Controls(50);
 controls.enableMovement();
 
-const roomModel = new Model('assets/models/cube-room/cube-room.gltf');
+const roomModel = new Model('assets/models/room/scene.gltf');
 roomModel.setPosition(0, 0, 0);
-roomModel.setScale(5, 1, 5);
+roomModel.setScale(1.2, 1, 1.2);
 roomModel.setRotation(0, 0, 0);
 
-const ezel = new Model('assets/models/blank-canvas/source/SMS-Canvas-Sketchfab/SMS Canvas.OBJ');
-ezel.setPosition(1, 1, 1);
-ezel.setScale(1, 1, 1);
-ezel.setRotation(0, 0, 0);
+const schilderij = new Model('assets/models/blank_canvas/scene.gltf');
+schilderij.setPosition(0, 1, -8.09);
+schilderij.setScale(1, 1, 1);
+schilderij.setRotation(0, 0, 0);
 
-const paintcan = new Model('assets/models/paint-can/source/bucket.OBJ');
-paintcan.setPosition(2, 2, 2);
-paintcan.setScale(1, 1, 1);
-paintcan.setRotation(0, 0, 0);
+const chair = new Model('assets/models/chair/scene.gltf');
+chair.setPosition(5, 0, 5);
+chair.setScale(0.07, 0.07, 0.07);
+chair.setRotation(0, 0, 0);
+
+const rust = new Model('assets/models/paint_can/scene.gltf');
+rust.setPosition(5, 1.1, 5);
+rust.setScale(0.07, 0.07, 0.07);
+rust.setRotation(0, 0, 0);
