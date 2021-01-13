69deb179db4bd6e07249fc4976701eaeeccdb70a
Bart <Bartdb2001@gmail.com>
2021-01-13 15:29:28 +0100

    Afbeeldingen staan nu allemaal in 1 folder

--- webpagina/app.js
+++ webpagina/app.js
@@ -14,21 +14,26 @@ roomModel.setRotation(0, 0, 0);
 
 const image = new XRImage('assets/images/FolkertdeJong.jpg');
 image.setPosition(0, 2, -8.11);
-image.setScale(2, 2, 2);
+image.setScale(1, 2, 2);
 
 const image2 = new XRImage('assets/images/Lisse-049.jpg');
 image2.setPosition(0, 2, 8.10);
 image2.setRotation(180, 0, 0)
 
 const chair = new Model('assets/models/chair/scene.gltf');
-chair.setPosition(5, 0, 5);
-chair.setScale(0.07, 0.07, 0.07);
+chair.setPosition(6, 0, 6);
+chair.setScale(0.06, 0.06, 0.06);
 chair.setRotation(0, 0, 0);
 
 const rust = new Model('assets/models/paint_can/scene.gltf');
-rust.setPosition(5, 1.1, 5);
-rust.setScale(0.07, 0.07, 0.07);
+rust.setPosition(6, 1, 6);
+rust.setScale(0.06, 0.06, 0.06);
 rust.setRotation(0, 0, 0);
+
+const ladder = new Model('assets/models/aluminium_ladder/scene.gltf');
+ladder.setPosition(-4, 0, -4);
+ladder.setScale(1.5, 1.5, 1.5);
+ladder.setRotation(0, 0, 0);
 
 // listen for an event
 // minecraftModel.addEventListener('click', () => console.log('je klikt op mij'));
