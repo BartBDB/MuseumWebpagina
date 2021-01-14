ee343963ab3ad857887a1434f423007679fb34fd
Bart <Bartdb2001@gmail.com>
2021-01-14 10:45:02 +0100

    extra tekst onder het WebXR component

--- webpagina/app.js
+++ webpagina/app.js
@@ -4,7 +4,7 @@ controls.enableMovement();
 
 const roomModel = new Model('assets/models/room/scene.gltf');
 roomModel.setPosition(0, 0, 0);
-roomModel.setScale(1.2, 0.6, 1.2);
+roomModel.setScale(0.8, 0.6, 1.2);
 roomModel.setRotation(0, 0, 0);
 
 //const schilderij = new Model('assets/models/blank_canvas/scene.gltf');
@@ -14,7 +14,7 @@ roomModel.setRotation(0, 0, 0);
 
 const image = new XRImage('assets/images/FolkertdeJong.jpg');
 image.setPosition(0, 2, -8.11);
-image.setScale(1, 2, 2);
+image.setScale(2, 2, 2);
 
 const image2 = new XRImage('assets/images/Lisse-049.jpg');
 image2.setPosition(0, 2, 8.10);
